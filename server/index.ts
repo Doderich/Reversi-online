3960f9c8d044d555d5b185753a2ce7252600d930
doderich <malte.budig@gmx.net>
2024-06-04 15:15:56 +0200

    changed the server

--- server/index.ts
+++ server/index.ts
@@ -14,7 +14,7 @@ const games: {game_id: string; players:{id: string; color: string;}[]; scores: {
 const { Server } = require('socket.io');
 const io = new Server(server,{
     cors: {
-    origin: "http://localhost:3000",
+    origin: process.env.ORIGIN,
     methods: ["GET", "POST"]
   }
 });
